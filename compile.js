b7eaa7b758f4e4d696b36fdd22ed3f73eaa20d14
calebbabin <object.caleb@gmail.com>
2022-02-09 12:19:35 -0600

    output a human readable HTML file

--- compile.js
+++ compile.js
@@ -28,4 +28,47 @@ output.sort((a, b) => {
     return 0;
 });
 
+// save out our JSON file
 fs.writeFileSync('build/index.json', JSON.stringify(output, null, 2));
+
+const categories = {};
+for (let index = 0; index < output.length; index++) {
+	const element = output[index];
+	if (!categories[element.metadata.category]) {
+		categories[element.metadata.category] = [];
+	}
+	categories[element.metadata.category].push(element);
+}
+
+let categoriesString = '';
+for (const category in categories) {
+	if (Object.hasOwnProperty.call(categories, category)) {
+		const element = categories[category];
+		categoriesString += `<h2>${category}</h2>\n`;
+		categoriesString += `<ul>\n`;
+		for (let index = 0; index < element.length; index++) {
+			const intro = element[index];
+			categoriesString += `<li><a href="${intro.metadata.src}.html">${intro.metadata.name}</a></li>\n`;
+		}
+		categoriesString += `</ul>\n`;
+	}
+}
+
+
+let HTMLOutput = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Twitch Chat Intros</title>
+</head>
+
+<body>
+	${categoriesString}
+</body>
+
+</html>`;
+
+fs.writeFileSync('build/index.html', HTMLOutput);
